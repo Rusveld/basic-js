24104df8497fd72d01ab8b8b56f9b4e685e4b2dd
Rusweld <sanekoir2015@gmail.com>
2021-09-09 21:16:17 +0300

    st-delete is done

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,7 +14,18 @@ import { NotImplementedError } from '../extensions/index.js';
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-export default function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function calculateHanoi(disksNumber, turnsSpeed ) {
+  let turns;
+  let seconds;
+  let count = 2;
+  for(let i = 1; i < disksNumber; i++){
+    count *= 2
+  }
+  turns = count - 1;
+  seconds = Math.floor((turns/turnsSpeed ) * 3600)
+  var obj = {
+    "turns": turns,
+    "seconds": seconds
+  }
+  return obj
 }
--- src/st-delete-digit.js
+++ src/st-delete-digit.js
@@ -11,7 +11,21 @@ import { NotImplementedError } from '../extensions/index.js';
  * For n = 152, the output should be 52
  *
  */
-export default function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function deleteDigit(n) {
+  n = String(n)
+  let m = n;
+  let counter = -Infinity;
+
+  for(let i = 0;i < n.length; i++){
+    n = m;
+
+    n = n.replace(n[i], "")
+    n = Number(n)
+ 
+    if(n>counter){
+      counter = n;
+    }
+    n = m;
+  }
+  return counter;
 }
