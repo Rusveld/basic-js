ccc4ac465c9bb74d8783b6c756c50c08cc0599bd
Rusweld <sanekoir2015@gmail.com>
2021-10-03 20:26:29 +0300

    chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,24 +5,33 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default {
+  chain:[],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.length
+    return this
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.chain.push( `( ${value} )`)
+    return this
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  removeLink(position) {
+    if(position > this.chain.length || position <= 0 || typeof position !== "number"){
+      this.chain = [];
+      throw new Error("You can't remove incorrect link!")
+    }
+
+    this.chain.splice(position-1, 1)
+    return this
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain = this.chain.reverse()
+    return this
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let lastchain = this.chain.join("~~")
+    this.chain = []
+    return lastchain
   }
 };
+
